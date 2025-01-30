32ef083226d5444da5d3af7c48200e81e29c718b
jazz-wong <wongj992@newschool.edu>
2025-01-29 19:12:43 -0500

    lol

---
+++ starx_master/starx/file_2/script.js
@@ -0,0 +1,17 @@
+function ClickGreyChangeColor() {
+    var square = document.getElementById("gray");
+    console.log("this is the fetched element", square);
+    square.style.backgroundColor = "";
+  }
+  
+  function setNewImage()
+  { 
+    document.getElementById("img1").src="https://cdn.glitch.global/57def00b-f9b9-4082-9838-7d16884d24e6/calendar_click.jpg?v=1669934446448";
+  }
+  
+  function setOldImage()
+  { 
+    document.getElementById("img1").src="https://cdn.glitch.global/57def00b-f9b9-4082-9838-7d16884d24e6/calendar_base.jpg?v=1669934438881";
+  }
+  
+  
