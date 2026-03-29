afe70dfd3f6678a3c0a6512f504bf2abeb682b54
jazz-wong <wongj992@newschool.edu>
2026-03-29 19:21:56 -0400

    fixed it

--- script.js
+++ script.js
@@ -8,58 +8,13 @@ function soundEffects() {
     const randomNum = getRandomInt(29);
     const audio = new Audio(`sound/${randomNum}.wav`);
     audio.play()
+    audio.volume = 0.5;
 }
+gsap.registerPlugin(ScrollToPlugin);
 
-        
+document.querySelectorAll("nav button").forEach((btn, index) => {
+    btn.addEventListener("click", () => {
+      gsap.to(".left", {duration: 1, scrollTo:{y:"#section" + (index + 1), offsetY:20}});
+    });
+  });
 
-       
-      
-
-    //   myAudioElement.addEventListener("canplaythrough", (event) => {
-    //     /* the audio is now playable; play it if permissions allow */
-    //     myAudioElement.play();
-    //   });
-
-      
-
-
-
-      
-    // function addEvent(){
-    //      document.body.addEventListener("click", play)
-    //    }
-      
-    // function play() {
-    //     var audio = document.getElementById("audio");
-    //     audio.play();
-    //   }
-   
-
-
-// function soundEffects(){
-//     document.body.addEventListener("click", play)
-//   }
-
-// function play() {
-//    var audio = document.getElementById("audio");
-//    audio.play();
-//  }
-
-//  function chooseSound() {
-   
-
-
-//     // const cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
-    
-//     for (let i = 0; i < randomNum.length; i++) {
-//         text += randomNum[i] + ".wav";
-//  }
-// }
-
-// //  var myPix = new Array("watch/watch1.png","watch/watch2.png","watch/watch3.png","watch/watch4.png");
- 
-// //  function choosePic() {
-// //       var randomNum = Math.floor(Math.random() * myPix.length);
-// //       document.getElementById("myPicture").src = myPix[randomNum]; 
-// //  }
-  
